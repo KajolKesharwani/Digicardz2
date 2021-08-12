ef8d739d5bab860c3ded7cbd11db6a90224d1c2f
Kajol <kajalkesharwani19@gmail.com>
2021-08-12 19:27:54 +0530

    initial commit

--- app/src/main/java/android/com/digicardz/Design3form.java
+++ app/src/main/java/android/com/digicardz/Design3form.java
@@ -1,5 +1,6 @@
 package android.com.digicardz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,21 +9,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Design3form extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
-    String spinner_text;
     SliderView sliderView;
-    TextView servicestxtv;
+    private EditText nametxt;
+    private EditText emailidtxt;
+    private EditText phnnotxt;
+    private TextView packagetxtview;
+    private TextView colortxtview;
+    private TextView servicestxtv;
+    private String spinner_text;
+    private EditText messagetxt;
+    private Button btn;
+    public String selected_val;
+
+
     boolean[] selectedday;
     ArrayList<Integer> servicelist = new ArrayList<>();
     String[] servicesarray = {"Call","Gmail","Whatsapp","Facebook","Instagram",
@@ -33,6 +52,14 @@ public class Design3form extends AppCompatActivity implements AdapterView.OnItem
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_design3form);
+
+        nametxt = findViewById(R.id.nametxt);
+        emailidtxt = findViewById(R.id.emailidtxt);
+        phnnotxt = findViewById(R.id.phnnotxt);
+        packagetxtview = findViewById(R.id.packagetxtview);
+        colortxtview = findViewById(R.id.colortxtview);
+        messagetxt = findViewById(R.id.messagetxt);
+
 
         Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Design3form.this,
@@ -46,7 +73,23 @@ public class Design3form extends AppCompatActivity implements AdapterView.OnItem
                 android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.d3colorpattern));
         myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner2.setAdapter(myAdapter2);
-        mySpinner2.setOnItemSelectedListener(this);
+        //  mySpinner2.setOnItemSelectedListener(this);
+        mySpinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?>arg0, View view, int arg2, long arg3) {
+
+                selected_val=mySpinner2.getSelectedItem().toString();
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
 
         sliderView = findViewById(R.id.image_slider);
         SliderAdapter sliderAdapter = new SliderAdapter(images);
@@ -112,6 +155,89 @@ public class Design3form extends AppCompatActivity implements AdapterView.OnItem
                 builder.show();
             }
         });
+
+
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Name = nametxt.getText().toString();
+                String EmailId = emailidtxt.getText().toString();
+                String PhoneNumber = phnnotxt.getText().toString();
+                String Package = spinner_text;
+                String Color_Pattern = selected_val;
+                String Service = servicestxtv.getText().toString();
+                String Message = messagetxt.getText().toString();
+                String Send = btn.getText().toString();
+                String subject = spinner_text;
+
+                // Toast.makeText(Contact.this, spinner_text, Toast.LENGTH_SHORT).show();
+
+                boolean check = validateinfo(Name, EmailId, PhoneNumber, Package, Color_Pattern, Service, Message, Send);
+
+                if (check == true) {
+
+                    Map<String, Object> v = new HashMap<>();
+                    v.put("name", Name);
+                    v.put("email", EmailId);
+                    v.put("Phone_no", PhoneNumber);
+                    v.put("Package", Package);
+                    v.put("Color_Pattern", Color_Pattern);
+                    v.put("Service", Service);
+                    v.put("Subject", subject);
+                    v.put("Message", Message);
+
+                    FirebaseFirestore.getInstance().collection("Form3").add(v).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentReference> task) {
+                            Toast.makeText(getApplicationContext(), "INSERTED", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+
+                }
+            }
+        });
+
+    }
+
+
+    private boolean validateinfo(String name, String emailId, String phoneNumber, String Package, String color_pattern, String service, String message, String send) {
+        if (name.length() == 0) {
+            nametxt.requestFocus();
+            nametxt.setError("FIELD CANNOT BE EMPTY");
+            return false;
+        } else if (!name.matches("[a-zA-Z]+")) {
+            nametxt.requestFocus();
+            nametxt.setError("ENTER ONLY ALPHABETICAL CHARACTER");
+            return false;
+        } else if (emailId.length() == 0) {
+            emailidtxt.requestFocus();
+            emailidtxt.setError("FIELD CANNOT BE EMPTY");
+            return false;
+        } else if (!emailId.matches("[a-zA-z0-9._-]+@[a-z]+\\.+[a-z]+")) {
+            emailidtxt.requestFocus();
+            emailidtxt.setError("ENTER VALID EMAIL");
+            return false;
+        } else if (phoneNumber.length() == 0) {
+            phnnotxt.requestFocus();
+            phnnotxt.setError("FIELD CANNOT BE EMPTY");
+            return false;
+        } else if (!phoneNumber.matches("^[0-9]{10,13}$")) {
+            phnnotxt.requestFocus();
+            phnnotxt.setError("correct Format: 91xxxxxxxxxx");
+            return false;
+        } else if (message.isEmpty()) {
+            messagetxt.requestFocus();
+            messagetxt.setError("Message Cannot Be Empty");
+            return false;
+        } else {
+            return true;
+        }
+
+
     }
 
     @Override
